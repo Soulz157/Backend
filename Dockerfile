58b78fc995a02c77900aae4e717bf2af01e7487e
Phoorich Maliwan <s6604062620026@email.kmutnb.ac.th>
2024-11-02 23:57:03 +0700

    swagger rest

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 
 
 COPY package.json .
-
+COPY ./src ./src
 COPY package-lock.json .
 
 RUN npm install
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const { PrismaClient, Problem } = require('@prisma/client')
 const prisma = new PrismaClient();
+var options = {
+    customCss: '.swagger-ui .topbar { display: none }'
+};
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+app.get('/', async (req, res) => {
+    res.send('Hello World');
+});
 
 app.get('/info/root', async (req, res) => {
     try {
@@ -78,29 +89,29 @@ app.get('/info/Interpolation', async (req, res) => {
 });
 
 
-// app.get(`/api`, async (req, res) => {
-//     try {
-//         const solution = await prisma.Simpson.findMany(
-//             {
-//                 where: {
-//                     Problem: "Differentiation"
-//                 }
+app.get(`/info/Integration`, async (req, res) => {
+    try {
+        const solution = await prisma.Simpson.findMany(
+            {
+                where: {
+                    Problem: "Integration"
+                }
 
-//             }
-//         )
-//         return res.status(200).json({
-//             result: true,
-//             status: "success",
-//             data: solution
-//         })
-//     } catch (error) {
-//         return res.status(400).json({
-//             result: false,
-//             status: "fail",
-//             msg: "ไม่พบข้อมูล",
-//         })
-//     }
-// });
+            }
+        )
+        return res.status(200).json({
+            result: true,
+            status: "success",
+            data: solution
+        })
+    } catch (error) {
+        return res.status(400).json({
+            result: false,
+            status: "fail",
+            msg: "ไม่พบข้อมูล",
+        })
+    }
+});
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
