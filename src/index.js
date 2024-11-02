99bfd7cc675ef8f318e98a5093623dc632420ba8
Phoorich Maliwan <s6604062620026@email.kmutnb.ac.th>
2024-11-02 14:34:06 +0700

    test

--- src/index.js
+++ src/index.js
@@ -77,29 +77,30 @@ app.get('/info/Interpolation', async (req, res) => {
     }
 });
 
-app.get(`/api`, async (req, res) => {
-    try {
-        const solution = await prisma.Simpson.findMany(
-            {
-                where: {
-                    Problem: "Differentiation"
-                }
+adadad
+// app.get(`/api`, async (req, res) => {
+//     try {
+//         const solution = await prisma.Simpson.findMany(
+//             {
+//                 where: {
+//                     Problem: "Differentiation"
+//                 }
 
-            }
-        )
-        return res.status(200).json({
-            result: true,
-            status: "success",
-            data: solution
-        })
-    } catch (error) {
-        return res.status(400).json({
-            result: false,
-            status: "fail",
-            msg: "ไม่พบข้อมูล",
-        })
-    }
-});
+//             }
+//         )
+//         return res.status(200).json({
+//             result: true,
+//             status: "success",
+//             data: solution
+//         })
+//     } catch (error) {
+//         return res.status(400).json({
+//             result: false,
+//             status: "fail",
+//             msg: "ไม่พบข้อมูล",
+//         })
+//     }
+// });
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
